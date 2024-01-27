996784bd6efbec21355f3e6e931804fa25c2365b
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2024-01-28 00:17:52 +0330

    css add

--- src/components/HowWork.jsx
+++ src/components/HowWork.jsx
@@ -6,33 +6,43 @@ import giveKey from "../image/give-key.png";
 function HowWork() {
   return (
     <div className={styles.allcontainer}>
-      <h1>How work ?</h1>
+      <div className={styles.header}>
+        <h1>How work ?</h1>
+        <p >For buying a car you should follow the three very simple steps :</p>
+      </div>
       <div className={styles.container}>
         <img src={select} alt="select car" />
-
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet,
-          aperiam ipsa recusandae veritatis officia molestiae dolores iusto ad
-          assumenda quod.
-        </p>
+        <div className={styles.title}>
+          <h2>1. Choose your car</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet,
+            aperiam ipsa recusandae veritatis officia molestiae dolores iusto ad
+            assumenda quod.
+          </p>
+        </div>
       </div>
       <div className={styles.container}>
         <img src={sales} alt="sales" />
-
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet,
-          aperiam ipsa recusandae veritatis officia molestiae dolores iusto ad
-          assumenda quod.
-        </p>
+        <div className={styles.title}>
+          <h2>2. Contact our Consultants</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet,
+            aperiam ipsa recusandae veritatis officia molestiae dolores iusto ad
+            assumenda quod.
+          </p>
+        </div>
       </div>
+
       <div className={styles.container}>
         <img src={giveKey} alt="give key" />
-
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet,
-          aperiam ipsa recusandae veritatis officia molestiae dolores iusto ad
-          assumenda quod.
-        </p>
+        <div className={styles.title}>
+          <h2>3.Contract and delivery of the car</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet,
+            aperiam ipsa recusandae veritatis officia molestiae dolores iusto ad
+            assumenda quod.
+          </p>
+        </div>
       </div>
     </div>
   );
